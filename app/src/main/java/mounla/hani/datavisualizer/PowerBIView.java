7d2128da253e898f135fe230f924f078cbe44643
Hani Mounla <hani-mounla@live.com>
2017-12-17 19:41:21 +0200

    Changed Power BI Report

--- app/src/main/java/mounla/hani/datavisualizer/PowerBIView.java
+++ app/src/main/java/mounla/hani/datavisualizer/PowerBIView.java
@@ -1,7 +1,7 @@
 package mounla.hani.datavisualizer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -13,13 +13,16 @@ public class PowerBIView extends AppCompatActivity {
         setContentView(R.layout.activity_power_biview);
 
         WebView webView = (WebView) findViewById(R.id.mainWebView);
-        webView.loadData("<iframe width=\"800\" height=\"600\" " +
-                        "src=\"https://app.powerbi.com/view?r=eyJrIjoiNmRjYmEwOGYtYjU2Ny00ZTAyLWIxNDAtNDM3MzY0YmEwZWZlIiwidCI6IjFkNWFhMmNmLTc4NGItNDU2MS05N2RmLWEyNmU3NzU3NGNjNCIsImMiOjl9\" " +
-                        "frameborder=\"0\" allowFullScreen=\"true\"></iframe>","text/html","utf-8");
+
+        String powerBI = "<iframe width=\"400\" height=\"800\" " +
+                "src=\"https://app.powerbi.com/view?r=eyJrIjoiNGRkN2Q0OGYtNjA5Zi00NTUzLThlYjQtYzlkZDIzZmM3YTBkIiwidCI6IjFkNWFhMmNmLTc4NGItNDU2MS05N2RmLWEyNmU3NzU3NGNjNCIsImMiOjl9\" " +
+                "frameborder=\"0\" allowFullScreen=\"true\"></iframe>";
+        webView.loadData(powerBI,"text/html","utf-8");
         WebSettings webViewSettings = webView.getSettings();
         webViewSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         webViewSettings.setJavaScriptEnabled(true);
         webViewSettings.setPluginState(WebSettings.PluginState.ON);
         webViewSettings.setBuiltInZoomControls(true);
+        webViewSettings.setDisplayZoomControls(false);
     }
 }
